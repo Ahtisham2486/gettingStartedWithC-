bd9beb8fd15506fe50cf189afe19d196443a0eac
Ahtisham2486 <lonesham86@gmail.com>
2024-10-29 18:08:17 +0530

    My first commit

--- c++/patterns/codeq/3.cpp
+++ c++/patterns/codeq/3.cpp
@@ -1,11 +1,11 @@
-#include<bits\stdc++.h>
-using namespace std;
-int count(cnt n)
-n = 343452;
-while (n>0){
-    cnt=cnt+1;
-    // nknkjnjk
-    n=n%10;
-}
-return cnt;
-}
+// #include<bits\stdc++.h>
+// using namespace std;
+// int count(int n)
+// n = 343452;
+// while (n>0){
+//     cnt=cnt+1;
+//     // nknkjnjk
+//     n=n%10;
+// }
+// return cnt;
+// }
---
+++ c++/patterns/codeq/main1.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"this is my first commit to the github profile:";
+    return 0;
+}
