51569ba05ff493d7a4515d3352ddc7dc327d9c41
Ahtisham2486 <lonesham86@gmail.com>
2024-12-07 13:06:25 +0530

    conditionals

---
+++ c++/conditionals/if_else.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+      int hour; 
+      cout<<"Enter the hour:"<< endl;
+      cin>> hour ;
+      if (hour >= 8 && hour <=18 ){
+        cout<<"The shop is open" << endl;}
+     else {
+        cout<<"shop is closed " <<endl;
+     }
+    
+ return 0;
+}
